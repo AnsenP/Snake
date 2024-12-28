23d3eebfd6f10ea119066eede6f4cf49643a2cb3
AnsenP <ansenpan169@gmail.com>
2024-12-28 15:09:22 +0800

    Add files via upload

---
+++ snake/Quartus_prj/ip_core/rom_pic_two/rom_pic_two_inst.v
@@ -0,0 +1,6 @@
+rom_pic_two	rom_pic_two_inst (
+	.address ( address_sig ),
+	.clock ( clock_sig ),
+	.rden ( rden_sig ),
+	.q ( q_sig )
+	);
---
+++ snake/Quartus_prj/ip_core/rom_pic_v/rom_pic_v_inst.v
@@ -0,0 +1,6 @@
+rom_pic_v	rom_pic_v_inst (
+	.address ( address_sig ),
+	.clock ( clock_sig ),
+	.rden ( rden_sig ),
+	.q ( q_sig )
+	);
